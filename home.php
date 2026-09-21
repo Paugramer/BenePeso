287f0b0cb86059a3a0308b4ef043753da115f448
Paugramer <pauloaguilarvidal@gmail.com>
2026-09-21 17:55:35 +0800

    Tighten mobile hero and section spacing

--- home.php
+++ home.php
@@ -114,7 +114,7 @@ try {
     <link rel="stylesheet" href="beneficiary_content_enhancements.css?v=1">
     <link rel="stylesheet" href="beneficiary_content_polish.css?v=9">
     <link rel="stylesheet" href="authenticated_experience.css?v=6">
-    <link rel="stylesheet" href="beneficiary_mobile.css?v=6">
+    <link rel="stylesheet" href="beneficiary_mobile.css?v=7">
     <script src="frontend_polish.js?v=20260921" defer></script>
     <script src="beneficiary_content_polish.js?v=1" defer></script>
 </head>
--- index.php
+++ index.php
@@ -152,7 +152,7 @@ if ($updated_stmt) {
     <link rel="stylesheet" href="frontend_polish.css?v=20260921">
     <link rel="stylesheet" href="beneficiary_responsive.css?v=10">
     <link rel="stylesheet" href="index.css?v=29">
-    <link rel="stylesheet" href="beneficiary_mobile.css?v=6">
+    <link rel="stylesheet" href="beneficiary_mobile.css?v=7">
 <script src="frontend_polish.js?v=20260921" defer></script>
 </head>
 <body class="public-index-page">
